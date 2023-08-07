d96127f2b793c6e09f6efad3885bb1353afa699e
Geuniv <kkoowl99@gmail.com>
2023-08-07 21:21:49 +0900

    2023.08.07 잔디심기 ( Spring 방학 과제 )

--- src/main/java/kopo/poly/controller/MainController.java
+++ src/main/java/kopo/poly/controller/MainController.java
@@ -2,11 +2,13 @@ package kopo.poly.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class MainController {
 
+    @GetMapping("/main")
     public String main() throws Exception {
         log.info(this.getClass().getName() + ".Main 페이지 보여주는 함수 실행");
         return "/main";
