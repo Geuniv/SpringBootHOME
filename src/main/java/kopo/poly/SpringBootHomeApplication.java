3bf68ea340359f9f0072184de873cfb00bb98b10
Geuniv <kkoowl99@gmail.com>
2023-08-07 21:23:47 +0900

    2023.08.07 잔디심기 ( Spring 방학 과제 )

---
+++ src/main/java/kopo/poly/SpringBootHomeApplication.java
@@ -0,0 +1,13 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHomeApplication.class, args);
+    }
+
+}
